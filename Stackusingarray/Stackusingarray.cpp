46c901f1043f58b3dee4240e25f73004cc80fc33
Lintangms14 <merayusukmalintang@gmail.com>
2023-06-15 10:32:38 +0700

    public

--- Stackusingarray/Stackusingarray.cpp
+++ Stackusingarray/Stackusingarray.cpp
@@ -8,3 +8,15 @@ private:
 	string stack_array[5];
 	int top;
 
+public:
+	Program() {
+		top = -1;
+	}
+
+	string push(string element) {
+		if (top == 4) {
+			cout << "Stack Full" << endl;
+			return "";
+		}
+
+		
